c13424a19ba6e38c95d82caadba4c85e7dc226aa
Emmi Schwitters <schwitme@rose-hulman.edu>
2019-02-06 16:06:35 -0500

    __repr__ method tested

--- src/m1_Point.py
+++ src/m1_Point.py
@@ -46,7 +46,7 @@ class Point(object):
         self.y = y
 
     def __repr__(self):
-        pass
+        return 'Point(' + str(self.x) + ', ' + str(self.y) +')'
 
     def clone(self):
         pass
@@ -219,13 +219,32 @@ def run_test_repr():
         print('Actual for p2:  ', p2)
     """
     # -------------------------------------------------------------------------
-    # TODO: 4.  Follow the same instructions as in _TODO_ 3 above,
+    # DONE: 4.  Follow the same instructions as in _TODO_ 3 above,
     #           but for the  __repr__  method specified above.
     # -------------------------------------------------------------------------
     print()
     print('-----------------------------------------------------------')
     print('Testing the   __repr__   method of the Point class.')
     print('-----------------------------------------------------------')
+
+    p1 = Point(30, 18)
+    print()
+    print('Expected for p1: Point(30, 18)')
+    print('Actual for p1:  ', p1)
+
+    p2 = Point(100, -40)
+    print()
+    print('Expected for p2: Point(100, -40)')
+    print('Actual for p2:  ', p2)
+    print('Expected for p1: Point(30, 18)')
+    print('Actual for p1:  ', p1)
+
+    p1.y = 999
+    print()
+    print('Expected for p1: Point(30, 999)')
+    print('Actual for p1:  ', p1)
+    print('Expected for p2: Point(100, -40)')
+    print('Actual for p2:  ', p2)
 
 
 def run_test_clone():
